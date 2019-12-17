d7fbaea47013ea7df12e6423504da9fd008b3702
Hu Nick <khu@adobe.com>
2019-12-17 22:03:56 +0800

    add test

--- src/test/java/com/ufs/campaign/UfsApplicationTests.java
+++ src/test/java/com/ufs/campaign/UfsApplicationTests.java
@@ -1,5 +1,6 @@
 package com.ufs.campaign;
 
+import com.ufs.campaign.smtp.SMTPEmailNotifier;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,12 @@ class UfsApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+
+    @Test
+    void sendEmail(){
+        SMTPEmailNotifier.send("testMail","khu@adobe.com","");
     }
 
 }
